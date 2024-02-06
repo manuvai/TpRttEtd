be3c63da2dd0fb0c482828e35c2ca9bf0a3ea0f3
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-02-06 22:34:44 +0100

    first commit

---
+++ src/main/java/dao/TestHibernate.java
@@ -0,0 +1,17 @@
+package dao;
+
+
+/**
+ * Classe de test pour Hibernate.
+ */
+public class TestHibernate
+{
+	/**
+	 * Programme de test.
+	 */
+	public static void main (String[] args)
+		{
+		System.out.println("Hibernate !");
+		}
+
+} /*----- Fin de la classe TestHibernate -----*/
